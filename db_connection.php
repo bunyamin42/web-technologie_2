1e31719f9908d328437b9ca2d0c225efd42a4e74
bunyamin42 <okur42@hotmail.de>
2023-09-29 17:14:47 +0200

    login begin

---
+++ db_connection.php
@@ -0,0 +1,18 @@
+<?php
+function getDbConnection() {
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "web-technologie";
+
+// Verbindung herstellen
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+// Die Verbindung prüfen
+
+if($connection->connect_error ){
+    die("Verbindung fehlgeschlagen: " . $connection->connect_error);
+
+
+}
+}
